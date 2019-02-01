98a295d6814e3259c9d66bca069b06a59867438e
Haoran-Zhao <zhaohaorandl@gmail.com>
2019-02-01 13:30:37 -0600

    code

--- matlab code/old/trajecotry_tracking/ILC_close_vairable.m
+++ matlab code/old/trajecotry_tracking/ILC_close_vairable.m
@@ -2,12 +2,12 @@ close all
 clear all
 %circle X Y Z
 wp = plotCircle([0 0 0], [0 0 0], 30);
-% wp = [-37.5000000000000,0,0;-36.5050000000000,0,-8.58000000000000;-32.7890000000000,0,-18.1900000000000;-28.8600000000000,0,-23.9300000000000;-21.7250000000000,0,-30.5660000000000;-12.4100000000000,0,-35.3800000000000;0,0,-37.5000000000000;8.75000000000000,0,-37.5000000000000;11.3200000000000,0,-37.1630000000000;14.9000000000000,0,-35.3800000000000;17.7280000000000,0,-31.9040000000000;18.7500000000000,0,-25.6980000000000;18.7500000000000,0,-18.7500000000000;18.7500000000000,0,-14.0150000000000;18.2850000000000,0,-6.98600000000000;13.4390000000000,0,-1.16800000000000;6.55000000000000,0,0.614000000000000;1.27000000000000,0,5.12200000000000;0,0,12.1430000000000;0,0,23.7500000000000;0,0,27.5000000000000;0,0.874000000000000,31.5800000000000;0,3.80000000000000,35.3400000000000;0,10,37.5000000000000;0,19,37.5000000000000;0,28.8000000000000,37.5000000000000;-0.548000000000000,43.2660000000000,37.5000000000000;-2.92900000000000,47.0710000000000,37.5000000000000;-7.15000000000000,49.5800000000000,37.5000000000000;-11.8160000000000,50,37.5000000000000;-19.6000000000000,50,37.5000000000000;-27.5000000000000,50,37.5000000000000;-31.3200000000000,49.2400000000000,37.5000000000000;-35.4800000000000,46.0270000000000,37.5000000000000;-37.2850000000000,42.0610000000000,37.5000000000000;-37.5000000000000,36.3700000000000,37.5000000000000;-37.5000000000000,19.7500000000000,37.5000000000000;-37.5000000000000,10,37.5000000000000;-37.5000000000000,3.80000000000000,35.3490000000000;-37.5000000000000,0.516000000000000,30.6700000000000;-37.5000000000000,0,20.7190000000000;-37.5000000000000,0,7.89000000000000];
-% wp = [wp(:,3),wp(:,1),wp(:,2)];
-% wp(end,:) = wp(1,:); %loop the trajectory
+wp = [-37.5000000000000,0,0;-36.5050000000000,0,-8.58000000000000;-32.7890000000000,0,-18.1900000000000;-28.8600000000000,0,-23.9300000000000;-21.7250000000000,0,-30.5660000000000;-12.4100000000000,0,-35.3800000000000;0,0,-37.5000000000000;8.75000000000000,0,-37.5000000000000;11.3200000000000,0,-37.1630000000000;14.9000000000000,0,-35.3800000000000;17.7280000000000,0,-31.9040000000000;18.7500000000000,0,-25.6980000000000;18.7500000000000,0,-18.7500000000000;18.7500000000000,0,-14.0150000000000;18.2850000000000,0,-6.98600000000000;13.4390000000000,0,-1.16800000000000;6.55000000000000,0,0.614000000000000;1.27000000000000,0,5.12200000000000;0,0,12.1430000000000;0,0,23.7500000000000;0,0,27.5000000000000;0,0.874000000000000,31.5800000000000;0,3.80000000000000,35.3400000000000;0,10,37.5000000000000;0,19,37.5000000000000;0,28.8000000000000,37.5000000000000;-0.548000000000000,43.2660000000000,37.5000000000000;-2.92900000000000,47.0710000000000,37.5000000000000;-7.15000000000000,49.5800000000000,37.5000000000000;-11.8160000000000,50,37.5000000000000;-19.6000000000000,50,37.5000000000000;-27.5000000000000,50,37.5000000000000;-31.3200000000000,49.2400000000000,37.5000000000000;-35.4800000000000,46.0270000000000,37.5000000000000;-37.2850000000000,42.0610000000000,37.5000000000000;-37.5000000000000,36.3700000000000,37.5000000000000;-37.5000000000000,19.7500000000000,37.5000000000000;-37.5000000000000,10,37.5000000000000;-37.5000000000000,3.80000000000000,35.3490000000000;-37.5000000000000,0.516000000000000,30.6700000000000;-37.5000000000000,0,20.7190000000000;-37.5000000000000,0,7.89000000000000];
+wp = [wp(:,3),wp(:,1),wp(:,2)];
+wp(end,:) = wp(1,:); %loop the trajectory
 %variables
 rangeInfluence = 5;
-iters = 5000; %control steps
+iters = 10000; %control steps
 spacingDist = 0.5; % in mm.  Make smaller to get more points
 var = 0.5; %variance of guassin
 control_hist = zeros(size(wp,1),1,3);
@@ -22,6 +22,7 @@ error_ch = NaN*ones(iters,1);
 error_list = NaN*ones(size(wp,1),100);
 error_ind = ones(size(wp,1),1);
 control_last = [0; 0; 0];
+orient_last = [0; 0; 0];
 % model: gravity pulls on our 2nd order system.
 x0 = [0,0,-40,0,0,0]';  %inital conditions: [x,y,z,vx,vy,vz]
 u = [0 0 0];
@@ -31,7 +32,7 @@ X(:,1)= x0;
 mg = 50; %N
 Dt = 0.02; %seconds
 Drag = 0.9;
-thrust  = 170; %N 120 140 150 200
+thrust  = 200; %N
 
 %cl pid term
 Kp =10;
@@ -71,7 +72,7 @@ hlead = plot3(wp(1,1),wp(1,2),wp(1,3),'bo','linewidth',1);
 hdirect = quiver3(X(1,1),X(2,1),X(3,1),u(1),u(2),u(3));
 % hILC = plot3(w2ILC(:,1),w2ILC(:,2),w2ILC(:,3),'m','linewidth',.2 );
 
-axis([-40 40 -40 40 -40 40])
+axis([-40 40 -40 40 -40 70])
 %axis equal
 htit = title('0');
 
@@ -87,7 +88,7 @@ hlead1 = plot3(wp(1,1),wp(1,2),wp(1,3),'bo','linewidth',1);
 hdirect1 = quiver3(X(1,1),X(2,1),X(3,1),u(1),u(2),u(3));
 % hILC = plot3(w2ILC(:,1),w2ILC(:,2),w2ILC(:,3),'m','linewidth',.2 );
 
-axis([-40 40 -40 40 -40 40])
+axis([-40 40 -40 40 -40 70])
 %axis equal
 htit1 = title('0');
 view([0 0 1])
@@ -104,7 +105,7 @@ hlead2 = plot3(wp(1,1),wp(1,2),wp(1,3),'bo','linewidth',1);
 hdirect2 = quiver3(X(1,1),X(2,1),X(3,1),u(1),u(2),u(3));
 % hILC = plot3(w2ILC(:,1),w2ILC(:,2),w2ILC(:,3),'m','linewidth',.2 );
 
-axis([-40 40 -40 40 -40 40])
+axis([-40 40 -40 40 -40 70])
 %axis equal
 htit2 = title('0');
 
@@ -115,6 +116,7 @@ herr_h = plot(Dt*(1:iters),error_h,'linewidth',1);
 hold on
 herr_ch = plot(Dt*(1:iters),error_ch,'linewidth',1);
 herr_lh = plot(Dt*(1:iters),error_lh,'linewidth',1);
+
 xlabel('time (s)');ylabel('error_m (mm)');
 %%%%%%%%%%%%%% END PLOTTING
 ind_last =1;
@@ -138,6 +140,7 @@ for i = 1: iters-1
     else
         error_m = mean(error(i-55:i)); 
     end
+    
     error_h(i) = error_m;
     error_ch(i) = error(i);
     error_lh(i) = norm(wp(ind_last,:)'-X(1:3,i));
@@ -147,7 +150,9 @@ for i = 1: iters-1
 %     if length(error_cpt) >1
 %     error_d = error_cpt(end-1)-error_cpt(end); %error deriviative
 %     end
-    errInt = errInt+ (lead_pt' - X(1:3,i)); %integral term     
+    
+    errInt = errInt+ (lead_pt' - X(1:3,i)); %integral term
+     
     % deriv term
     if i ==1
         vel = [0;0;0];
@@ -158,20 +163,26 @@ for i = 1: iters-1
 %     learningRate = exp(-error_m/T); %simulated annealing
 
     p2l_vector = wp(ind,:)-X(1:3,i)';
-    orient = p2l_vector/norm(p2l_vector);
     learningRate1 = tanh(norm((lead_pt' - X(1:3,i))));
     learningRate2 = tanh(norm((cp - X(1:3,i))));
 %     control_ILC = [control_hist(ind,1,1);control_hist(ind,1,2);control_hist(ind,1,3)] + learningRate.*(lead_pt' - X(1:3,i));
-    control_ILC = control_last + learningRate1.*(lead_pt' - X(1:3,i))/norm((lead_pt' - X(1:3,i)))+learningRate2.*(cp - X(1:3,i))/norm((cp - X(1:3,i)));
-    control_ILC = control_ILC/norm(control_ILC);
+    control_ILC = control_last + learningRate1.*norm(lead_pt' - X(1:3,i))+learningRate2.*norm(cp - X(1:3,i));
+%     orient_ILC = orient_last+ learningRate1.*(lead_pt' - X(1:3,i))/norm((lead_pt' - X(1:3,i)))+learningRate2.*(cp - X(1:3,i))/norm((cp - X(1:3,i)));
+    orient_ILC = orient_last+ learningRate1.*(lead_pt' - X(1:3,i))/norm((lead_pt' - X(1:3,i)))+learningRate2.*(cp - X(1:3,i))/norm((cp - X(1:3,i)));
+
+    %orient_ILC = orient_ILC/norm(orient_ILC);
     control_cl = Kp*(lead_pt' - X(1:3,i)) + Ki*errInt - Kd*vel;
-    control = control_cl + control_ILC; % PID type ILC
-    control_hist(ind,1,:) = control ; 
+    if (norm(control_ILC)+norm(control_cl))>thrust
+        control = thrust * ((orient_ILC+control_cl)/norm(control_cl+orient_ILC));
+    else
+        control = (norm(control_ILC)+norm(control_cl))*((orient_ILC+control_cl)/norm(control_cl+orient_ILC)); % PID type ILC
+    end
+    %control_hist(ind,1,:) = control ; 
     % controller steers the thrust (only controls the orientation)
-    u = thrust*(control/norm(control));
-%     disp(u)
-    control_last = control;
-    
+    u = control;
+    disp(u)
+    control_last = norm(control);
+    orient_last = ((orient_ILC+control_cl)/norm(control_cl+orient_ILC));
     
     
     X(:,i+1) = [ ...
@@ -205,6 +216,7 @@ for i = 1: iters-1
         set(herr_h,'Ydata',error_h);
         set(herr_ch,'Ydata',error_ch);
         set(herr_lh,'Ydata',error_lh);
+
         drawnow
     end
     
